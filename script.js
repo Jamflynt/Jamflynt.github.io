e540e2a114de240b0bfb583bfb233558a17a231e
Jamie Flynt <jamflynt@gmail.com>
2021-09-30 14:37:35 -0400

    Navbar functionality complete.

--- script.js
+++ script.js
@@ -32,13 +32,15 @@ navSlide();
         sections.forEach( section => {
             const sectionTop = section.offsetTop;
             const sectionHeight = section.clientHeight;
-            if(pageYOffset >= sectionTop){
+            if(pageYOffset >= (sectionTop - sectionHeight / 3)){
                 current = section.getAttribute('id')
             }
         })
-        console.log(current);
-        // currentLine.forEach(li=> {
-        //     li.classList.remove('current'); 
 
-        // })
-    })
+        navLi.forEach(li=> {
+            li.classList.remove('active'); 
+            if(li.classList.contains(current)){
+                li.classList.add("active")
+            } 
+        })
+    });
